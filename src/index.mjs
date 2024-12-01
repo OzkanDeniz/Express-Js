352bfa20ec9c124f4b017994091283b01c03aced
ozkan <ozkandnz09@gmail.com>
2024-12-01 14:03:23 +0300

    query parameters

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express, { request, response } from "express";
 
 const app = express();
 
@@ -7,21 +7,27 @@ const PORT = process.env.PORT || 3000;
 const mockUsers = [
   { id: 1, username: "anson", displayName: "Anson" },
   { id: 2, username: "jack", displayName: "Jack" },
-  { id: 3, username: "john", displayName: "John" },
+  { id: 3, username: "adam", displayName: "Adam" },
+  { id: 4, username: "tina", displayName: "Tina" },
+  { id: 5, username: "jason", displayName: "Jason" },
+  { id: 6, username: "henry", displayName: "Henry" },
+  { id: 7, username: "marilyn", displayName: "Merilyn" },
 ];
 
-app.get("/", (req, res) => {
-  res.status(201).send({ msg: "Hello!" });
-});
+//GET REQUEST
+// app.get("/", (req, res) => {
+//   res.status(201).send({ msg: "Hello!" });
+// });
 
-app.get("/api/users", (req, res) => {
-  res.send(mockUsers);
-});
+// app.get("/api/users", (req, res) => {
+//   res.send(mockUsers);
+// });
 
-app.get("/api/products", (req, res) => {
-  res.send([{ id: 123, username: "chicken breast", price: 12.99 }]);
-});
+// app.get("/api/products", (req, res) => {
+//   res.send([{ id: 123, username: "chicken breast", price: 12.99 }]);
+// });
 
+//ROUTE PARAMETERS (req.params (/api/users/:id))
 app.get("/api/users/:id", (req, res) => {
   console.log(req.params);
   const parseID = parseInt(req.params.id);
@@ -33,10 +39,22 @@ app.get("/api/users/:id", (req, res) => {
   return res.send(findUser);
 });
 
+//QUERY PARAMETERS (localhost300/products?key=value&key2=value2)
+app.get("/api/users", (req, res) => {
+  console.log(req.query);
+
+  const {
+    query: { filter, value },
+  } = req;
+  //when filter and value undefined
+  if (!filter && !value) return res.send(mockUsers);
+  res.send(mockUsers);
+});
+
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
 });
 
 //localhost300
 //localhost300/users
-//localhost300/products
+//localhost300/products?key=value&key2=value2
